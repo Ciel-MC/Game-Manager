9c72808359be669cd72c80aede8f32a7031aafcf
Shuaiouke <47144701+Shuaiouke@users.noreply.github.com>
2022-01-03 16:11:20 +0800

    Initial Commit

---
+++ src/main/java/hk/eric/gamemanager/GameManager.java
@@ -0,0 +1,17 @@
+package hk.eric.gamemanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GameManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
